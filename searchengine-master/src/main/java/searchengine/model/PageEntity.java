00507df0073755b3e1c8e28a3858a0ad68c6f11d
invassiion <126901070+invassiion@users.noreply.github.com>
2023-06-09 15:59:39 +0700

    Update PageEntity.java
    Edit PageEntity

--- searchengine-master/src/main/java/searchengine/model/PageEntity.java
+++ searchengine-master/src/main/java/searchengine/model/PageEntity.java
@@ -16,7 +16,6 @@ public class PageEntity {
     @Column( nullable = false)
     private int id;
 
-
     @Column(columnDefinition = "TEXT NOT NULL")
     private String path;
 
@@ -24,23 +23,6 @@ public class PageEntity {
     private int code;
 
     @Column( columnDefinition = "MEDIUMTEXT")
-
-    @ManyToOne(targetEntity = SiteEntity.class)
-    @JoinColumn(name = "site_id", nullable = false, insertable = false,updatable = false)
-    private SiteEntity siteId;
-
-    @OneToMany(mappedBy = "page", cascade = CascadeType.REMOVE)
-    private List<searchengine.model.Indexx> indexxes;
-
-    @Column(columnDefinition = "TEXT NOT NULL, UNIQUE KEY pathIndex (path(512),site_id)")
-    private String path;
-
-
-    @Column( nullable = false, columnDefinition = "VARCHAR(255)")
-    private int code;
-
-    @Column( columnDefinition = "MEDIUMTEXT NOT NULL")
-
     private String content;
 
     @Column(name = "site_id", nullable = false)
