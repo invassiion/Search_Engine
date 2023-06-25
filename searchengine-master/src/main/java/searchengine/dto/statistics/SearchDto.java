d88ebb809446c69e90ab20edb2825f64ba313d13
invassiion <invassiion@yandex.ru>
2023-06-25 16:34:02 +0700

    ошибка UnsatisfiedDependencyException.  PageRepository.ByLemma

---
+++ searchengine-master/src/main/java/searchengine/dto/statistics/SearchDto.java
@@ -0,0 +1,24 @@
+package searchengine.dto.statistics;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SearchDto  implements Comparable<SearchDto>{
+    private String site;
+    private String siteName;
+    private String uri;
+    private String title;
+    private String snippet;
+    private Float relevance;
+
+
+    @Override
+    public int compareTo(SearchDto o) {
+        return this.getRelevance().compareTo(o.getRelevance());
+    }
+}
